1b0de4e7a8e761a679444ef21b196b3199be634b
PaTTeX <g00406870@atu.ie>
2024-11-17 19:45:19 +0000

    comments for App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar';
-import Header from './components/header';
-import Footer from './components/footer';
-import Content from './components/content';
-import Read from './components/read';
-import Create from './components/create';
+// Import required libraries and components
+import 'bootstrap/dist/css/bootstrap.min.css';  // Import Bootstrap for styling
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // React Router for navigation
+import NavigationBar from './components/NavigationBar';  // Navigation bar component
+import Header from './components/header';  // Header component
+import Footer from './components/footer';  // Footer component
+import Content from './components/content';  // Main content component
+import Read from './components/read';  // Component for reading/displaying movies
+import Create from './components/create';  // Component for creating new movies
 
 function App() {
   return (
-    <Router>
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Content />} />
-        <Route path="/read" element={<Read />} />
-        <Route path="/create" element={<Create />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-}
+    <Router>  {/* Set up React Router to handle navigation */}
+      <NavigationBar />  {/* Render the navigation bar */}
 
-export default App;
+      <Routes>  {/* Define routes for different paths */}
+        <Route path="/" element={<Content />} />  {/* Route for the homepage */}
+        <Route path="/read" element={<Read />} />  {/* Route for reading/displaying movies */}
+        <Route path="/create" element={<Create />} />  {/* Route for creating new movies */}
+      </Routes>
+
+      <Footer />  {/* Render the footer */}
+    <
