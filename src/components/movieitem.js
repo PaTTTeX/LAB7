6945e2eadbd3969d11ed4dad68d6b96ec463ab3c
PaTTeX <g00406870@atu.ie>
2024-11-17 19:45:44 +0000

    comments for movieitem.js

--- src/components/movieitem.js
+++ src/components/movieitem.js
@@ -1,18 +1,24 @@
-import { useEffect } from "react";
-import Card from 'react-bootstrap/Card';
+// Import necessary libraries and components
+import { useEffect } from "react";  // useEffect to run side effects
+import Card from 'react-bootstrap/Card';  // Card component from Bootstrap for styling
 
-const MovieItem = (props)=> {
+const MovieItem = (props) => {
+  
+  // Effect to log movie details when the movie prop changes
   useEffect(() => {
-    console.log("Movie Item:", props.mymovie);
-  }, [props.mymovie]); // Only run this effect when the mymovie prop changes
+    console.log("Movie Item:", props.mymovie);  // Log the movie object passed in as a prop
+  }, [props.mymovie]);  // Only rerun the effect when 'mymovie' changes
 
   return (
     <div>
+      {/* Bootstrap Card to display movie details */}
       <Card>
-        <Card.Header>{props.mymovie.Title}</Card.Header>
+        <Card.Header>{props.mymovie.Title}</Card.Header>  {/* Movie title */}
         <Card.Body>
           <blockquote className="blockquote mb-0">
-            <img src={props.mymovie.Poster} alt={props.mymovie.Title} />
+            {/* Display the movie poster */}
+            <img src={props.mymovie.Poster} alt={props.mymovie.Title} /> 
+            {/* Movie year */}
             <footer>{props.mymovie.Year}</footer>
           </blockquote>
         </Card.Body>
@@ -21,4 +27,4 @@ const MovieItem = (props)=> {
   );
 }
 
-export default MovieItem;
+export default MovieItem;  // Export the MovieItem component for use in other parts of the app
--- src/components/read.js
+++ src/components/read.js
@@ -1,29 +1,33 @@
-import Movies from "./movies";
-import { useEffect, useState } from "react";
-import axios from "axios";
+// Import necessary libraries
+import Movies from "./movies";  // Import Movies component to display movies list
+import { useEffect, useState } from "react";  // useState for state, useEffect for side effects
+import axios from "axios";  // Axios for making HTTP requests
 
 const Read = () => {
 
-  const [movies, setMovies] = useState([]);
+  // State to store the list of movies
+  const [movies, setMovies] = useState([]);  // Initially, an empty array
 
+  // Fetch movies data when the component mounts
   useEffect(() => {
-    
+    // Send GET request to the backend to retrieve movies
     axios.get('http://localhost:4000/api/movies')
       .then((response) => {
-        console.log(response.data);
-        setMovies(response.data.movies);
+        console.log(response.data);  // Log the response from server
+        setMovies(response.data);  // Update the state with the movie list
       })
       .catch((error) => {
-        console.log(error);
+        console.log(error);  // Log any errors that occur
       });
-  });
+  }, []);  // Empty dependency array to only run once when the component mounts
 
   return (
     <div>
-      <h3>Hello from read component!</h3>
+      <h3>Hello from read component!</h3>  {/* Title of the page */}
+      {/* Pass the list of movies to the Movies component */}
       <Movies myMovies={movies} />
     </div>
   );
 }
 
-export default Read;
+export default Read;  // Export the Read component for use elsewhere in the app
